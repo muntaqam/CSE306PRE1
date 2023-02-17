b51e08218f3ed423e4f85ec647e18d7d17d16452
bjkent4 <bjkent4@buffalo.edu>
2023-02-17 13:53:44 -0500

    made some changes, still working on mean

--- csv.c
+++ csv.c
@@ -2,37 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 
-  //don't forget to close file with fclose(newFile)
-int mean(int col, char *file){
-    FILE *inputFile = fopen(file, "r"); 
-    char buffer[1024]; //used to store data that fgets() reads
-    double total = 0;
-    double divisor = 0;
+void mean_field(int col, char *inFile){
+  FILE *file = fopen(inFile, "r");
+  if (inFile == NULL) {
+    printf("Error opening file %s\n", inFile);
+    return;
+  }
+  char buffer[1024]; //used to store data that fgets() reads
+  double total = 0;
+  double divisor = 0;
     
-     while (fgets(buffer, 1024, inputFile)){
-       char *rowValue = strtok(buffer, ","); //strtok split the input on the comma
-       //total += atoi(buffer);
-       //divisor++;
-       //puts(buffer);
-       while (rowValue != NULL){
-	 
-       printf("%s\n", rowValue);
-       rowValue = strtok(NULL, ",");
-       }
-     }//end while
+  while (fgets(buffer, 1024, file)){
+    char *rowValue = strtok(buffer, "\","); //strtok split the input on the comma
+    total += atoi(rowValue);
+    divisor++;
+    //puts(buffer);
+    while (rowValue != NULL){
+ 
+      //printf("%s\n", rowValue);
+      rowValue = strtok(NULL, "\",");
+    }
+  }//end while
+     
+  printf("%f\n", total/divisor);
+  fclose(file);
+ }//end mean_field
 
-     fclose(inputFile);
-     //printf("mean is %f\n", total/divisor);
-     return total/divisor;
- }//end mean
+void records_field(int field, double value, char *inFile){
+  FILE *file = fopen(inFile, "r");
+  if (inFile == NULL) {
+    printf("Error opening file %s\n", inFile);
+    return;
+  }
+  printf("test\n");
+  fclose(file);
+}//end records_field
 
 int main(int argc, char *argv[]){
   for (int i = 0; i < argc; i++){
     if (strcmp("-mean", argv[i]) == 0){  //strcmp compares str values, ret 0 if match
-      mean(atoi(argv[i+1]), argv[argc-1]);
-    }
-  }//end for
-
+      mean_field(atoi(argv[i+1]), argv[argc-1]);
+    }else{
+    if (strcmp("-records", argv[i]) == 0){
+      records_field(atoi(argv[i+1]), atoi(argv[i+2]), argv[argc-1]);
+		   }
+	    }//end else
+    }//end for
   return 0;
 }//end main
 
