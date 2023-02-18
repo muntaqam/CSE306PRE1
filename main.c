b1bbf4b4c5a7b4bde65501d40762590ea7101945
bjkent4 <bjkent4@buffalo.edu>
2023-02-17 20:22:37 -0500

    merge and update from branch bjkent4

--- main.c
+++ main.c
@@ -106,13 +106,57 @@ void csv_min(FILE* fp,int min_field){
 
 }
 
+//mean field Display the arithmetic mean (average) value in
+//the indicated field of the data records. The first field is
+//numbered 0. This option ignores non-numeric data in the indicated
+//field. If there is no numeric data in the indicated field in any
+//of the data records the program exits with error code EXIT_FAILURE.
+//See how fields are identified when the -h option is used.
 
+void mean_field(int col, char *inFile){
+  FILE *file = fopen(inFile, "r");
+  if (inFile == NULL) {
+    printf("Error opening file %s\n", inFile);
+    exit(EXIT_FAILURE);
+  }
+  char buffer[1024]; //used to store data that fgets() reads
+  double total = 0;
+  double divisor = 0;
+  
+  if (!fgets(buffer, 1024, file)){
+    printf("Empty File.\n");
+    exit(EXIT_FAILURE);
+  }
 
+  while (fgets(buffer, sizeof buffer, file) != NULL){
+    char *row = buffer;
+    row = strtok(buffer, "\"");
+    while (row != NULL){      
+      //puts(row);
+      total += atof(row);
+      divisor++;
+      row = strtok(NULL, "\"");
+     }	   
+  }//end while 
+  
+  printf("%f\n", total/divisor);
+  fclose(file);
+ }//end mean_field
 
-
-
-
-
+//-records field value Display the records from file containing
+//value in the the indicated field. The first field is numbered 0.
+//Value is formatted according to the same rules as data in fields.
+//See how fields are identified when the -h option is used.
+ 
+void records_field(int field, double value, char *inFile){
+  FILE *file = fopen(inFile, "r");
+  if (inFile == NULL) {
+    printf("Error opening file %s\n", inFile);
+    return;
+  }
+  printf("test\n");
+  fclose(file);
+}//end records_field
 
 
 int main(int argc, char *argv[]) {
